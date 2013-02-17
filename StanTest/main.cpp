ee5f58038d43857887b9e3bce73c020a5c74761d
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2013-02-17 15:48:15 -0800

    ...aaaand stan's automatic differentiation automagically works with my templatized probability code. sweeeeet.

--- StanTest/main.cpp
+++ StanTest/main.cpp
@@ -1,3 +1,5 @@
+#include "../Common/Distributions.h"
+
 #include <stan/model/prob_grad_ad.hpp>
 #include <stan/gm/command.hpp>
 #include <stan/prob/distributions/univariate/continuous/normal.hpp>
@@ -25,10 +27,31 @@ private:
 	double mean, stddev;
 };
 
+class MyTestModel : public model::prob_grad_ad
+{
+public:
+	MyTestModel(double mu, double sigma)
+		: model::prob_grad_ad(1), mean(mu), stddev(sigma) {}
+	agrad::var log_prob(
+		std::vector<agrad::var>& params_r, 
+		std::vector<int>& params_i,
+		std::ostream* output_stream = 0)
+	{
+		agrad::var lp__(0.0);
+		//lp__ += prob::normal_log(params_r[0], mean, stddev);
+		lp__ += log(simference::Math::Probability::NormalDistribution<agrad::var>::Prob(params_r[0], mean, stddev));
+		return lp__;
+	}
+
+private:
+	double mean, stddev;
+};
+
 int main(int argc, char** argv)
 {
 	// Create a model
-	TestModel m(0.0, 1.0);
+	//TestModel m(0.0, 1.0);
+	MyTestModel m(0.0, 1.0);
 
 	// Create a sampler
 	auto sampler = mcmc::nuts<>(m);
